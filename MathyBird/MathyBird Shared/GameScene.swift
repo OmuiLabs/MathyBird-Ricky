ac430fe6e2a09e466643844ccb707fc570c935e5
dev <ricky.pan@omuilabs.com>
2023-09-28 15:37:17 -0500

    Se agregan las imagenes en el juego

--- MathyBird/MathyBird Shared/GameScene.swift
+++ MathyBird/MathyBird Shared/GameScene.swift
@@ -6,120 +6,58 @@
 //
 
 import SpriteKit
+import Foundation
+import SwiftUI
 
 class GameScene: SKScene {
     
+    var fondo = SKSpriteNode()
     
-    fileprivate var label : SKLabelNode?
-    fileprivate var spinnyNode : SKShapeNode?
-
+    var player = SKSpriteNode()
+    var cubo = SKSpriteNode()
+    var tuboBot = SKSpriteNode()
+    var tuboTop = SKSpriteNode()
     
-    class func newGameScene() -> GameScene {
-        // Load 'GameScene.sks' as an SKScene.
-        guard let scene = SKScene(fileNamed: "GameScene") as? GameScene else {
-            print("Failed to load GameScene.sks")
-            abort()
-        }
+    
+    override func didMove(to view: SKView){
         
-        // Set the scale mode to scale to fit the window
-        scene.scaleMode = .aspectFill
+        fondo = SKSpriteNode(imageNamed: "fondo")
+        player = SKSpriteNode(imageNamed: "magicarp")
+        cubo = SKSpriteNode(imageNamed: "block")
+        tuboBot = SKSpriteNode(imageNamed: "tuboG")
+        tuboTop = SKSpriteNode(imageNamed: "tuboG")
         
-        return scene
-    }
-    
-    func setUpScene() {
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        fondo.setScale(0.6)
+        fondo.zPosition = -1
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        player.position = CGPoint(x: -450, y: 0)
+        player.setScale(0.6)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 4.0
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    override func didMove(to view: SKView) {
-        self.setUpScene()
-    }
-
-    func makeSpinny(at pos: CGPoint, color: SKColor) {
-        if let spinny = self.spinnyNode?.copy() as! SKShapeNode? {
-            spinny.position = pos
-            spinny.strokeColor = color
-            self.addChild(spinny)
-        }
-    }
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-}
-
-#if os(iOS) || os(tvOS)
-// Touch-based event handling
-extension GameScene {
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        tuboBot.position = CGPoint(x: 0, y: -200)
+        tuboBot.setScale(0.6)
         
-        for t in touches {
-            self.makeSpinny(at: t.location(in: self), color: SKColor.green)
-        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches {
-            self.makeSpinny(at: t.location(in: self), color: SKColor.blue)
-        }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches {
-            self.makeSpinny(at: t.location(in: self), color: SKColor.red)
-        }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches {
-            self.makeSpinny(at: t.location(in: self), color: SKColor.red)
-        }
-    }
-    
-   
-}
-#endif
+        tuboTop.position = CGPoint(x: 0, y: 250)
+        tuboTop.zRotation = Angle(degrees: 180).radians
+        tuboTop.setScale(0.6)
+        
+        cubo.position = CGPoint(x: 0, y: 25)
+        cubo.setScale(0.6)
 
-#if os(OSX)
-// Mouse-based event handling
-extension GameScene {
-
-    override func mouseDown(with event: NSEvent) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        self.makeSpinny(at: event.location(in: self), color: SKColor.green)
+        self.addChild(fondo)
+        self.addChild(player)
+        self.addChild(cubo)
+        self.addChild(tuboTop)
+        self.addChild(tuboBot)
     }
     
-    override func mouseDragged(with event: NSEvent) {
-        self.makeSpinny(at: event.location(in: self), color: SKColor.blue)
+    override func touchesBegan(_ touches: Set <UITouch>, with event: UIEvent?){
+        
     }
     
-    override func mouseUp(with event: NSEvent) {
-        self.makeSpinny(at: event.location(in: self), color: SKColor.red)
+    override func update(_ currentTime: CFTimeInterval){
+        
     }
+    
 
 }
-#endif
 
--- MathyBird/MathyBird iOS/GameViewController.swift
+++ MathyBird/MathyBird iOS/GameViewController.swift
@@ -14,15 +14,22 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene.newGameScene()
-
-        // Present the scene
-        let skView = self.view as! SKView
-        skView.presentScene(scene)
-        
-        skView.ignoresSiblingOrder = true
-        skView.showsFPS = true
-        skView.showsNodeCount = true
+        if let view = self.view as! SKView?{
+            if let scene = SKScene(fileNamed: "GameScene"){
+                scene.scaleMode = .aspectFill
+                
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            view.showsPhysics = true
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+    
+    override var shouldAutorotate: Bool{
+        return true
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
@@ -32,6 +39,7 @@ class GameViewController: UIViewController {
             return .all
         }
     }
+
 
     override var prefersStatusBarHidden: Bool {
         return true

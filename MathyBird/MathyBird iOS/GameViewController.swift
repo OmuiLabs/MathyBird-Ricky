0f384f7d62be99d93ae7157916d6c0adff8f36f0
dev <ricky.pan@omuilabs.com>
2023-09-28 11:48:57 -0500

    initial commit

---
+++ MathyBird/MathyBird iOS/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  MathyBird iOS
+//
+//  Created by Ricky Pan on 09/28/23.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
